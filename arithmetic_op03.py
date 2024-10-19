ffc08024860bc81140823d1feab559a95875c921
Mehrojlatipov <mehrojlatifov06@gmail.com>
2024-10-19 22:19:36 +0500

    done _03

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,5 +1,6 @@
 #"n" deb nomlangan o'zgaruvchi yarating va unga ikki xonali raqamni belgilang
-
+n = 42
 #n sonining teskarisini toping va uni "answer" deb nomlangan o'zgaruvchiga belgilang.
-
+answer = (n % 10) * 10 + (n // 10)
 #"answer" qiymatini chop eting
+print (answer)
