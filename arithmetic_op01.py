0b80e8ee411b40f006af14f88f3a9da2d72c4cf9
Mehrojlatipov <mehrojlatifov06@gmail.com>
2024-10-19 09:05:13 +0500

    done_01

--- arithmetic_op01.py
+++ arithmetic_op01.py
@@ -1,7 +1,8 @@
 #"x" o'zgaruvchisini yarating va unga 10 qiymatini belgilang.
-
+x = 10
 #"y" o'zgaruvchisini yarating va unga 5 qiymatini belgilang.
-
+y = 5
 #"Natija" o'zgaruvchisini yarating va unga x + y qiymatini belgilang.
-
+natija = x + y
 #"Natija" qiymatini chop eting.
+print (natija)
