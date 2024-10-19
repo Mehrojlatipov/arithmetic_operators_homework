eea60b3bd0ab70b3d99c97b9ed42f1904be4123b
Mehrojlatipov <mehrojlatifov06@gmail.com>
2024-10-19 23:02:35 +0500

    done_06

--- arithmetic_op06.py
+++ arithmetic_op06.py
@@ -1,5 +1,6 @@
 #"Raqam" deb nomlangan o'zgaruvchi yarating va unga int turdagi ning istalgan son qiymatini belgilang.
-
+raqam = 122
 #Raqamni 2 ga bo'ling va raqamga taminlang.
-
+raqam = raqam // 2
 #Raqamni chop eting.
+print (raqam)
