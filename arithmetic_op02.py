d4dc8c1a97bf06561a70283fe3424374ccadaa65
Mehrojlatipov <mehrojlatifov06@gmail.com>
2024-10-19 09:28:25 +0500

    done_02

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -1,3 +1,6 @@
 #"n" o'zgaruvchisini yarating va unga 43 qiymatini belgilang.
+n = 43
 #"answer" o'zgaruvchisini yarating va unga raqam raqamlari yig'indisini belgilang.
+answer = (43 // 10) + (43 % 10)
 #"answer" qiymatini chop eting.
+print (answer)
