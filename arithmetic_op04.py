1c490564acde9fae7506385fe258c2f13be75a89
Mehrojlatipov <mehrojlatifov06@gmail.com>
2024-10-19 22:31:26 +0500

    done_04

--- arithmetic_op04.py
+++ arithmetic_op04.py
@@ -1,11 +1,12 @@
 #"Raqam" deb nomlangan o'zgaruvchi yarating va unga uch xonali raqamni belgilang.
-
+raqam = 123
 #"Raqam"ning birinchi raqamini toping va x1 ga belgilang.
-
+x1 = (raqam % 100)
 #"Raqam"ning ikkinchi raqamini toping va x2 ga belgilang.
-
+x2 = (raqam % 10)
 #"Raqam"ning uchinchi raqamini toping va x3 ga belgilang.
-
+x3 = (raqam % 1)
 #"Javob" deb nomlangan o'zgaruvchi yarating va unga x1, x2, x3 uchta raqam yig'indisini belgilang.
-
+javob = x1 + x2 + x3
 #Javobning qiymatini chop eting.
+print (javob)
