bc98b931fbd0116743f4160c7b3e56e28f001a6e
Mehrojlatipov <mehrojlatifov06@gmail.com>
2024-10-19 22:49:38 +0500

    done_5

--- arithmetic_op05.py
+++ arithmetic_op05.py
@@ -1,5 +1,6 @@
 #Int ma'lumotlar turi bilan "raqam" deb nomlangan o'zgaruvchi yarating.
-
+raqam = 15
 #"Javob" deb nomlangan o'zgaruvchi yarating va unga sonning 3 ga bo'linishining qolgan qismini belgilang.
-
+javob = raqam % 4
 #"Javob"ni chop eting.
+print (javob)
